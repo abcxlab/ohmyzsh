c54e550d74594e578a075dd79326fdb20f17f58e
Qiang <chiangzi@qq.com>
2020-06-06 14:36:43 +0800

    refine code

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 sudo apt -y install zsh
 #使用国内github repo加速
 export REMOTE=https://gitlab.abcxlab.com/github/ohmyzsh.git 
-sh -c "$(curl -fsSL https://gitlab.abcxlab.com/github/ohmyzsh/-/raw/master/tools/install.sh)"
+curl -fsSL https://gitlab.abcxlab.com/github/ohmyzsh/-/raw/master/tools/install.sh | sh -
 #基于默认主题，客制my主题
 cp ~/.oh-my-zsh/themes/robbyrussell.zsh-theme ~/.oh-my-zsh/themes/myrobbyrussell.zsh-theme
 echo 'PROMPT="%{$fg_bold[green]%}%n@%M %(?:%{$fg_bold[green]%}➜ :%{$fg_bold[red]%}➜ )"' >> ~/.oh-my-zsh/themes/myrobbyrussell.zsh-theme
 echo "PROMPT+='%{\$fg[cyan]%}%c%{\$reset_color%} \$(git_prompt_info)'" >> ~/.oh-my-zsh/themes/myrobbyrussell.zsh-theme
-sed -i 's/robbyrussell/myrobbyrussell/g' ~/.zshrc
-source ~/.zshrc
-
+sed -i 's/\"robbyrussell/\"myrobbyrussell/g' ~/.zshrc
+sudo chsh -s $(which zsh)
+zsh
